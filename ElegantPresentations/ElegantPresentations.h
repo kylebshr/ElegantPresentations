0fbcaa6d3ea403ff16328649ec645441c4b39a39
Kyle Bashour <kylebshr@me.com>
2016-02-21 14:03:24 -0500

    Initial Commit

---
+++ ElegantPresentations/ElegantPresentations.h
@@ -0,0 +1,19 @@
+//
+//  ElegantPresentations.h
+//  ElegantPresentations
+//
+//  Created by Kyle Bashour on 2/21/16.
+//  Copyright © 2016 Kyle Bashour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ElegantPresentations.
+FOUNDATION_EXPORT double ElegantPresentationsVersionNumber;
+
+//! Project version string for ElegantPresentations.
+FOUNDATION_EXPORT const unsigned char ElegantPresentationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ElegantPresentations/PublicHeader.h>
+
+
